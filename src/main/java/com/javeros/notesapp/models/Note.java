f9540f740fb938a4c9ee87e125171e56ec90ca69
Eric Suarez <ericfernandomou5@gmail.com>
2022-03-03 18:26:30 -0600

    algunos cambios antes de empezar

--- src/main/java/com/javeros/notesapp/models/Note.java
+++ src/main/java/com/javeros/notesapp/models/Note.java
@@ -1,34 +1,73 @@
 package com.javeros.notesapp.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-@Entity
-@Table(name = "notes")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Entity
+//@Table(name = "notes")
 public class Note implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private static final long serialVersionUID = 1L;
+
+	//@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 30)
+    //@Column(nullable = false, length = 30)
     private String title;
 
-    @Column(length = 255)
+    //@Column(length = 255)
     private String description;
 
-    @Column(name = "create_at")
+    //@Column(name = "create_at")
     private LocalDate createAt;
 
-    @PrePersist
+    //@PrePersist
     public void init(){
         this.createAt = LocalDate.now();
     }
+    
+    public Note() {}
+
+	public Note(Long id, String title, String description, LocalDate createAt) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.createAt = createAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDate createAt) {
+		this.createAt = createAt;
+	}
+    
+    
 }
--- src/main/java/com/javeros/notesapp/models/User.java
+++ src/main/java/com/javeros/notesapp/models/User.java
@@ -1,26 +1,55 @@
 package com.javeros.notesapp.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
-@Table(name = "users")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Entity
+//@Table(name = "users")
 public class User implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private static final long serialVersionUID = 1L;
+
+	//@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 15)
+    //@Column(nullable = false, length = 15)
     private String username;
 
-    @Column(nullable = false, length = 255)
+    //@Column(nullable = false, length = 255)
     private String password;
+    
+    public User() {}
+
+	public User(Long id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
 }
